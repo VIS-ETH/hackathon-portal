37d26707574fe66b16e0dd6d4a6e1c6c1f8d95b9
Ramon Wick <ramon.wick@inf.ethz.ch>
2024-09-23 11:31:39 +0200

    new traits

--- backend/Readme.md
+++ backend/Readme.md
@@ -3,5 +3,5 @@
 ## Database ORM generation
 The structs are generated automatically from the database with the following command:
 ```shell
-sea-orm-cli generate entity --database-url $POSTGRES_URL -o src/entity --model-extra-derives Serialize,Deserialize,ToSchema
+sea-orm-cli generate entity --database-url $POSTGRES_URL -o src/entity --with-serde both --model-extra-derives 'utoipa::ToSchema' --enum-extra-derives 'utoipa::ToSchema'   
  ```
--- backend/src/entity/role_assignment.rs
+++ backend/src/entity/role_assignment.rs
@@ -2,8 +2,11 @@
 
 use super::sea_orm_active_enums::Role;
 use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize, ToSchema)]
+#[derive(
+    Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize, utoipa :: ToSchema,
+)]
 #[sea_orm(table_name = "role_assignment")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
