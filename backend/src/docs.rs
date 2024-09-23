a01994619667d1c877b8bbb1fcd0ecc2c4a30067
Ramon Wick <ramon.wick@inf.ethz.ch>
2024-09-23 11:31:28 +0200

    swagger setup

---
+++ backend/src/docs.rs
@@ -0,0 +1,11 @@
+use utoipa::OpenApi;
+use utoipauto::utoipauto;
+
+#[utoipauto(paths = "src/api")]
+#[derive(OpenApi)]
+#[openapi(
+    tags(
+        (name = "Hackathon Dashboard", description = "Swagger for the Hackathon Dashboard"),
+    ),
+)]
+pub struct ApiDocs;
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod docs;
+
+
+use std::net::SocketAddr;
+use axum::{routing::get, Json};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+use crate::docs::ApiDocs;
+
+
+
+// #[derive(OpenApi)]
+// #[openapi(paths(openapi))]
+// struct ApiDoc;
+
+/// Return JSON version of an OpenAPI schema
+// #[utoipa::path(
+//     get,
+//     path = "/api-docs/openapi.json",
+//     responses(
+//         (status = 200, description = "JSON file", body = ())
+//     )
+// )]
+// async fn openapi() -> Json<utoipa::openapi::OpenApi> {
+//     Json(ApiDoc::openapi())
+// }
+
+#[tokio::main]
+async fn main() {
+    let socket_address: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let listener = tokio::net::TcpListener::bind(socket_address).await.unwrap();
+
+    let app = axum::Router::new()
+        // .route("/api-docs/openapi.json", get(openapi));
+        .merge(SwaggerUi::new("/api/docs").url("/api/docs/openapi.json", ApiDocs::openapi()));
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap()
 }
