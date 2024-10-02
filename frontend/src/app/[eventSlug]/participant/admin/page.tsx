8c64828e7cc44bd90fa8a0ae4700228fe0d6bf5a
Ramon Wick <ramon.wick@inf.ethz.ch>
2024-10-02 23:20:22 +0200

    event settings

---
+++ frontend/src/app/[eventSlug]/participant/admin/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { useGetEvent } from "@/api/gen";
+import EventForm from "@/componentes/EventForm";
+import { Stack, Title, Text, Tabs } from "@mantine/core";
+import { IconCalendarEvent, IconListDetails, IconMessageCircle, IconPhoto, IconSettings, IconUsers } from "@tabler/icons-react";
+import { useParams } from "next/navigation";
+
+
+export default function Page () {
+    const {eventSlug} = useParams<{eventSlug: string}>();
+
+    const event_id = "fae4d7ff-ee08-4e16-8802-a1b1797145d5"
+    const {data: event} = useGetEvent(event_id);
+
+  return (
+    <Stack gap={"md"}>
+        
+      <Title order={1}>Admin Interface</Title>
+      <Tabs defaultValue="gallery">
+      <Tabs.List>
+        <Tabs.Tab value="gallery" leftSection={<IconSettings  />}>
+          Event Settings
+        </Tabs.Tab>
+        <Tabs.Tab value="messages" leftSection={<IconUsers />}>
+          User Management Seminar
+        </Tabs.Tab>
+        <Tabs.Tab value="settings" leftSection={<IconListDetails/>}>
+          Project Assignment
+        </Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="gallery" my={"md"}>
+      {event && <EventForm  event={event}/>}
+      </Tabs.Panel>
+
+      <Tabs.Panel value="messages">
+        Messages tab content
+      </Tabs.Panel>
+
+      <Tabs.Panel value="settings">
+        Settings tab content
+      </Tabs.Panel>
+    </Tabs>
+      
+    </Stack>
+  )
+}
+
