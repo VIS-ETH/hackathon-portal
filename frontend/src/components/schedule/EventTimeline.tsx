e9e164bf844d3b6782cad79c92214ab4444152df
Hannes Eberhard <heberhard@student.ethz.ch>
2024-10-13 06:35:50 +0200

    Add toggle to filter past events

--- frontend/src/components/schedule/EventTimeline.tsx
+++ frontend/src/components/schedule/EventTimeline.tsx
@@ -1,8 +1,11 @@
 import EventTimelineItem from "./EventTimelineItem";
 
 import { Appointment } from "@/api/gen/schemas";
+import { cardProps } from "@/styles/common";
 
-import { Timeline } from "@mantine/core";
+import { useState } from "react";
+
+import { Card, Switch, Text, Timeline } from "@mantine/core";
 
 type EventTimelineProps = {
   appointments: Appointment[];
@@ -15,17 +18,45 @@ const EventTimeline = ({
   manage,
   refetch,
 }: EventTimelineProps) => {
+  const [now] = useState(new Date());
+  const [showAll, setShowAll] = useState(false);
+
+  // Events will be shown for 30 minutes after they end
+  const grace = 1000 * 60 * 30;
+
+  const isInPast = (timestamp: string) => {
+    const date = new Date(`${timestamp}Z`);
+    return date.getTime() < now.getTime() - grace;
+  };
+
+  const filteredAppointments = appointments.filter((appointment) => {
+    return showAll || !isInPast(appointment.end ?? appointment.start);
+  });
+
   return (
-    <Timeline lineWidth={2}>
-      {appointments.map((appointment) => (
-        <EventTimelineItem
-          key={appointment.id}
-          appointment={appointment}
-          manage={manage}
-          refetch={refetch}
-        />
-      ))}
-    </Timeline>
+    <>
+      <Switch
+        label={`Show all (${appointments.length})`}
+        checked={showAll}
+        onChange={(event) => setShowAll(event.currentTarget.checked)}
+      />
+      {filteredAppointments.length ? (
+        <Timeline lineWidth={2}>
+          {filteredAppointments.map((appointment) => (
+            <EventTimelineItem
+              key={appointment.id}
+              appointment={appointment}
+              manage={manage}
+              refetch={refetch}
+            />
+          ))}
+        </Timeline>
+      ) : (
+        <Card {...cardProps} style={{ borderStyle: "dashed" }}>
+          <Text>No appointments found</Text>
+        </Card>
+      )}
+    </>
   );
 };
 
